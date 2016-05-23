b80debdfd23b03ab9a735972a7bf123fcecc8c2c
Don <heihuwudi@gmail.com>
2016-05-23 14:45:20 +0800

    Update index.js
    修复一个page出现多个uml的情况

--- index.js
+++ index.js
@@ -3,6 +3,55 @@ var fs = require('fs');
 var mkdirp = require('mkdirp');
 var crypto = require('crypto');
 var path = require('path');
+var blockRegex = /^```uml((.*[\r\n]+)+?)?```$/img;
+
+function processBlockList(page, umlPath) {
+    var basePath = '/images/uml/';
+    var dirPath = basePath + path.dirname(page.path);
+    var baseName = path.basename(page.path, '.md');
+    // console.log(dirPath);
+    var linkBase = dirPath + '/' + baseName;
+    // console.log(linkBase);
+    var match;
+    var index = 0;
+    while ((match = blockRegex.exec(page.content))) {
+        var indexLinkBase = linkBase + '_' + (index++);
+        var umlPath = '.' + indexLinkBase + '.uml';
+        console.log('processing uml... %j %j', page.path, umlPath);
+        var rawBlock = match[0];
+        var blockContent = match[1];
+        mkdirp.sync(path.dirname(umlPath));
+        fs.exists(umlPath, function(exists) {
+            if (!exists) {
+                fs.writeFileSync(umlPath, blockContent);
+            }
+        })
+        var lastmd5sum = ''
+        try {
+            lastmd5sum = fs.readFileSync(umlPath + '.sum');
+        } catch (e) {}
+        var md5sum = crypto.createHash('sha1').update(blockContent).digest('hex');
+        var isUpdateImageRequired = (lastmd5sum != md5sum);
+        //UML
+        if (isUpdateImageRequired) {
+            fs.writeFileSync(umlPath, blockContent);
+            fs.writeFileSync(umlPath + '.sum', md5sum);
+            debugger;
+            try {
+                execFile('java', [
+                    //'-Dapple.awt.UIElement=true',
+                    '-jar', 'plantuml.jar',
+                    //'-nbthread auto',
+                    //'-tsvg',
+                    '-charset', 'UTF-8',
+                    umlPath, '-o', '.'
+                ]);
+            } catch (e) {};
+        }
+        page.content = page.content.replace(rawBlock, '![](' + indexLinkBase + '.png)');
+    }
+    return page;
+}
 
 function parseUml(page, dirPath, umlPath) {
     uml = page.content.match(/^```uml((.*[\r\n]+)+?)?```$/img);
@@ -17,33 +66,21 @@ function parseUml(page, dirPath, umlPath) {
 
 function execFile(command, args, callback) {
     var prc = spawn(command, args);
-
-    prc.on('error', function (err) {
+    prc.on('error', function(err) {
         console.log('cannot spawn java');
     });
-
     prc.stdout.on('data', function(data) {
         console.log(data.toString());
     });
-
     prc.stderr.on('data', function(data) {
         console.log(data.toString());
     });
-
     prc.on('close', function(code) {
-        if ("function" === typeof callback) callback(!!code);
+        if ("function" === typeof callback) callback( !! code);
     });
 };
-
 module.exports = {
     book: {
-        assets: "./book",
-        js: [
-            "test.js"
-        ],
-        css: [
-            "test.css"
-        ],
         html: {
             "html:start": function() {
                 return "<!-- Start book " + this.options.title + " -->"
@@ -60,110 +97,7 @@ module.exports = {
         }
     },
     hooks: {
-        // For all the hooks, this represent the current generator
-
-        // This is called before the book is generated
-        "init": function() {
-            console.log("init gitbook-plugin-plantuml!");
-        },
-
-        // This is called after the book generation
-        "finish": function() {
-            console.log("finish gitbook-plugin-plantuml!");
-        },
-
-        // The following hooks are called for each page of the book
-        // and can be used to change page content (html, data or markdown)
-
-
         // Before parsing markdown
-        "page:before": function(page) {
-            // page.path is the path to the file
-            // page.content is a string with the file markdown content
-            var basePath = '/images/uml/';
-            var dirPath = basePath + path.dirname(page.path);
-            var baseName = path.basename(page.path, '.md');
-            // console.log(dirPath);
-            var linkBase = dirPath + '/' + baseName;
-            // console.log(linkBase);
-            var umlPath = '.' + linkBase + '.uml';
-
-            var hasUml = parseUml(page, dirPath, umlPath);
-            if (!hasUml) { return page; }
-            
-            console.log('processing uml... %j', page.path);
-
-            var text = fs.readFileSync(umlPath, 'utf8');
-
-            var md5sum = crypto.createHash('sha1').update(text).digest('hex');
-
-            var lastmd5sum = ''
-
-            try {
-                lastmd5sum = fs.readFileSync(umlPath + '.sum');
-            }
-            catch (e) {
-            }
-
-            var isUpdateImageRequired = (lastmd5sum != md5sum);
-
-            fs.writeFileSync(umlPath + '.sum', md5sum);
-
-            var lines = text.split('```,');
-
-            //console.log('%j', lines);
-
-            //UML
-            if (isUpdateImageRequired) {
-                debugger;
-                try {
-                    execFile('java', [
-                        //'-Dapple.awt.UIElement=true',
-                        '-jar',
-                        'plantuml.jar',
-                        //'-nbthread auto',
-                        //'-tsvg',
-                        '-charset',
-                        'UTF-8',
-                        umlPath,
-                        '-o',
-                        '.'
-                    ]);
-                } catch (e) {};
-            }
-
-            for (var i = 0; i < lines.length; i++) {
-                var line = lines[i];
-                if (i < (lines.length-1)) {
-                    line += '```';
-                }
-                if (i == 0) {
-                    page.content = page.content.replace(line, '![](' + linkBase + '.png)');
-                    continue;
-                }
-                if (i < 10) {
-                    page.content = page.content.replace(line, '![](' + linkBase + '_00' + i + '.png)');
-                    continue;
-                }
-                if (i >= 10 && i < 100) {
-                    page.content = page.content.replace(line, '![](' + linkBase + '_0' + i + '.png)');
-                    continue;
-                }
-                if (i >= 100) {
-                    page.content = page.content.replace(line, '![](' + linkBase + '_' + i + '.png)');
-                    continue;
-                }
-            };
-            //page.content = page.content.replace(/^```uml((.*\n)+?)?```$/g, '');
-            // Example:
-            //page.content = "# Title\n" + page.content;
-
-            return page;
-        },
-
-        // Before html generation
-        "page": function(page) {
-            return page;
-        }
+        "page:before": processBlockList
     }
 };
