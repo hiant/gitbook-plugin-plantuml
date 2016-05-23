fd782000d3c746b1cb59afb9cc12f654783d12f5
hiant <heihuwudi@gmail.com>
2016-05-24 00:38:31 +0800

    update

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 var spawn = require('child_process').spawn;
-var fs = require('fs');
-var mkdirp = require('mkdirp');
+var fs = require('fs')
+var fse = require('fs-extra')
 var crypto = require('crypto');
 var path = require('path');
-var copyDir = require("copy-dir");
 var config;
 var blockRegex = /^```uml((.*[\r\n]+)+?)?```$/im;
-var basePath = '/images/uml/';
+var basePath = './uml/';
 
 function processBlockList(page, umlPath) {
-    var dirPath = basePath + path.dirname(page.path);
+    var dirPath = path.dirname(page.path);
     var baseName = path.basename(page.path, '.md');
-    // console.log(dirPath);
-    var linkBase = dirPath + '/' + baseName;
-    // console.log(linkBase);
     var match;
     var index = 0;
     while ((match = blockRegex.exec(page.content))) {
-        var indexLinkBase = linkBase + '_' + (index++);
-        var umlPath = '.' + indexLinkBase + '.uml';
-        console.log('processing uml... %j %j', page.path, umlPath);
+        var indexBaseName = baseName + '_' + (index++);
+        var umlBasePath = basePath + dirPath + '/' + indexBaseName;
+        var umlPath = umlBasePath + '.uml';
+        var pngBaseName = indexBaseName + '.png';
+        var pngPath = dirPath + '/' + pngBaseName;
+        console.log(umlPath);
+        console.log(pngPath);
         var rawBlock = match[0];
         var blockContent = match[1];
-        mkdirp.sync(path.dirname(umlPath));
         var isUpdateImageRequired = !fs.existsSync(umlPath);
         var md5sum;
         if (!isUpdateImageRequired) {
@@ -38,10 +37,16 @@ function processBlockList(page, umlPath) {
                 isUpdateImageRequired = true;
             }
         }
+        var to = '_book/' + dirPath + '/' + pngBaseName;
+        if(!isUpdateImageRequired) {
+            isUpdateImageRequired = !fs.existsSync(to);
+        }
         //UML
+        console.log('%j-> %j-> %j %j', page.path, umlPath, pngPath, isUpdateImageRequired);
         if (isUpdateImageRequired) {
-            fs.writeFileSync(umlPath, blockContent);
-            fs.writeFileSync(umlPath + '.sum', md5sum, encoding = 'utf-8');
+            fse.mkdirsSync(path.dirname(umlPath));
+            fse.outputFileSync(umlPath, blockContent);
+            fse.outputFileSync(umlPath + '.sum', md5sum, encoding = 'utf-8');
             debugger;
             try {
                 execFile('java', [
@@ -52,9 +57,13 @@ function processBlockList(page, umlPath) {
                     '-charset', 'UTF-8',
                     umlPath, '-o', '.'
                 ]);
+                var from = umlBasePath + '.png';
+                console.log(from);
+                fse.copySync(from, to);
+                fse.copySync(from, dirPath);
             } catch (e) {};
         }
-        page.content = page.content.replace(rawBlock, '![](' + indexLinkBase + '.png)');
+        page.content = page.content.replace(rawBlock, '![](' + pngBaseName + ')');
     }
     return page;
 }
@@ -98,7 +107,7 @@ module.exports = {
         "page:before": processBlockList,
         "finish": function() {
             // 最终写入索引数据
-            copyDir.sync('.' + basePath, path.join(config.output , './images/uml'));
+            // 
         }
     }
 };
