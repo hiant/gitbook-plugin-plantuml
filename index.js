44a74bad05288ad6e4e54bbd06938e9b1e7a2327
hiant <heihuwudi@gmail.com>
2016-05-24 12:59:47 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-var spawn = require('child_process').spawn;
-var fs = require('fs')
-var fse = require('fs-extra')
-var crypto = require('crypto');
-var path = require('path');
-var config;
-var blockRegex = /^```uml((.*[\r\n]+)+?)?```$/im;
-var basePath = './uml/';
+const execFileSync = require('child_process').execFileSync;
+const fs = require('fs')
+const fse = require('fs-extra')
+const crypto = require('crypto');
+const path = require('path');
+const blockRegex = /^```uml((.*[\r\n]+)+?)?```$/im;
+const basePath = 'images/uml/';
 
 function processBlockList(page, umlPath) {
     var dirPath = path.dirname(page.path);
     var baseName = path.basename(page.path, '.md');
+    var outputDir = '_book/images/uml/' + dirPath;
     var match;
     var index = 0;
     while ((match = blockRegex.exec(page.content))) {
         var indexBaseName = baseName + '_' + (index++);
-        var umlBasePath = basePath + dirPath + '/' + indexBaseName;
-        var umlPath = umlBasePath + '.uml';
-        var pngBaseName = indexBaseName + '.png';
-        var pngPath = dirPath + '/' + pngBaseName;
-        console.log(umlPath);
-        console.log(pngPath);
+        var assetsPathPrefix = basePath + dirPath + '/' + indexBaseName;
+        var linkPath = assetsPathPrefix + '.png';
+        var umlPath = './' + assetsPathPrefix + '.uml';
+        var pngPath = './' + linkPath;
         var rawBlock = match[0];
         var blockContent = match[1];
         var isUpdateImageRequired = !fs.existsSync(umlPath);
@@ -37,9 +35,8 @@ function processBlockList(page, umlPath) {
                 isUpdateImageRequired = true;
             }
         }
-        var to = '_book/' + dirPath + '/' + pngBaseName;
-        if(!isUpdateImageRequired) {
-            isUpdateImageRequired = !fs.existsSync(to);
+        if (!isUpdateImageRequired) {
+            isUpdateImageRequired = !fs.existsSync(pngPath);
         }
         //UML
         console.log('%j-> %j-> %j %j', page.path, umlPath, pngPath, isUpdateImageRequired);
@@ -47,67 +44,27 @@ function processBlockList(page, umlPath) {
             fse.mkdirsSync(path.dirname(umlPath));
             fse.outputFileSync(umlPath, blockContent);
             fse.outputFileSync(umlPath + '.sum', md5sum, encoding = 'utf-8');
-            debugger;
             try {
-                execFile('java', [
-                    //'-Dapple.awt.UIElement=true',
+                execFileSync('java', [
                     '-jar', 'node_modules/gitbook-plugin-plantuml/plantuml.jar',
-                    //'-nbthread auto',
-                    //'-tsvg',
                     '-charset', 'UTF-8',
                     umlPath, '-o', '.'
                 ]);
-                var from = umlBasePath + '.png';
-                console.log(from);
-                fse.copySync(from, to);
-                fse.copySync(from, dirPath);
-            } catch (e) {};
+                fse.mkdirsSync(outputDir);
+                //console.log(outputDir + '/' + indexBaseName + '.png');
+                fse.copySync(pngPath, outputDir + '/' + indexBaseName + '.png');
+            } catch (e) {
+                console.log(e);
+            };
         }
-        page.content = page.content.replace(rawBlock, '![](' + pngBaseName + ')');
+        page.content = page.content.replace(rawBlock, '![](' + linkPath + ')');
     }
     return page;
 }
 
-function execFile(command, args, callback) {
-    var prc = spawn(command, args);
-    prc.on('error', function(err) {
-        console.log('cannot spawn java');
-    });
-    prc.stdout.on('data', function(data) {
-        console.log(data.toString());
-    });
-    prc.stderr.on('data', function(data) {
-        console.log(data.toString());
-    });
-    prc.on('close', function(code) {
-        if ("function" === typeof callback) callback( !! code);
-    });
-}
-
 module.exports = {
-    book: {
-        html: {
-            "html:start": function() {
-                return "<!-- Start book " + this.options.title + " -->"
-            },
-            "html:end": function() {
-                return "<!-- End of book " + this.options.title + " -->"
-            },
-            "head:start": "<!-- head:start -->",
-            "head:end": "<!-- head:end -->",
-            "body:start": "<!-- body:start -->",
-            "body:end": "<!-- body:end -->"
-        }
-    },
     hooks: {
-        "init": function() {
-            config = this.options;
-        },
         // Before parsing markdown
-        "page:before": processBlockList,
-        "finish": function() {
-            // 最终写入索引数据
-            // 
-        }
+        "page:before": processBlockList
     }
 };
